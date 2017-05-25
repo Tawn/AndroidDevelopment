f12ab352bdb9f4990c132fd43ab24c2f0c0686d5
Thanh Kha <Tawn@users.noreply.github.com>
2017-05-25 17:33:42 -0400

    Check with instructor

---
+++ HelloThanh/README.txt
@@ -0,0 +1,8 @@
+Thanh Kha '19
+Assignment01
+Bobby
+
+ASSIGNMENT DESCRIPTION
+
+Assignment01 provided guidance on how create a new project and visualization of a simple program, that includes "Hello Thanh". This ensured that I was able to install the Android Studio. On top of that, to make sure I have the proper setup of the SDK and the virtual device emulator. 
+
---
+++ HelloThanh/app/src/main/java/assignment01/csc214/hellothanh/HelloThanhActivity.java
@@ -0,0 +1,13 @@
+package assignment01.csc214.hellothanh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloThanhActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_thanh);
+    }
+}
